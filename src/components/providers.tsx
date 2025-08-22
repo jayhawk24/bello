7d639db36f9fe5b6a75557cd17ea24983aefbe30
Anubhav Gupta <anubhavthrilling24@gmail.com>
2025-08-22 13:56:52 +0530

    feat: remove register

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -15,26 +15,7 @@ export function Providers({ children }: ProvidersProps) {
             <NotificationProvider>
                 {children}
                 <Toaster
-                    position="top-right"
-                    toastOptions={{
-                        duration: 4000,
-                        style: {
-                            background: '#363636',
-                            color: '#fff',
-                        },
-                        success: {
-                            duration: 3000,
-                            style: {
-                                background: '#10b981',
-                            },
-                        },
-                        error: {
-                            duration: 5000,
-                            style: {
-                                background: '#ef4444',
-                            },
-                        },
-                    }}
+                    position="bottom-right"
                 />
             </NotificationProvider>
         </SessionProvider>
